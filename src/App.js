2d283eb95215c0d51bf87324a6b8bbbcae35ef7d
azhilus <tomar3007shivansh@gmail.com>
2024-02-07 00:10:28 +0530

    Home Page Completion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import React from 'react';
+import About from './components/about/About';
+import Home from './components/home/Home';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+import Services from './components/services/Services';
+import Sidebar from './components/sidebar/Sidebar';
+import Contact from './components/contact/Contact';
+import Testimonials from './components/testimonials/Testimonials';
+import Pricing from './components/pricing/Pricing';
+import Blog from './components/blog/Blog';
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+    <Sidebar />
+    <main className='main'>
+      <Home />
+      <About />
+      <Services />
+      <Resume />
+      <Portfolio />
+      <Pricing />
+      <Testimonials />
+      <Blog />
+      <Contact />
+    </main>
+    </>
   );
 }
 
---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="https://www.instagram.com/azhilus" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="https://www.twitter.com/azhilus" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-twitter"></i>
+        </a>
+
+        <a href="https://www.behance.com/shivanshtomar" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-behance"></i>
+        </a>
+
+        <a href="https://www.dribbble.com/azhilus" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-dribbble"></i>
+        </a>
+
+        <a href="https://www.artstation.com/azhilus" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-artstation"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
